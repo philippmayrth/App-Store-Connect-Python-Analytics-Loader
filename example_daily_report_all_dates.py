6e2f6d417f36901e10f27d074419701f3bd56978
Philipp Mayr <me@philipp-mayr.de>
2019-02-02 21:45:08 +0100

    major improvement

--- example_daily_report_all_dates.py
+++ example_daily_report_all_dates.py
@@ -4,7 +4,7 @@ import calendar
 
 
 def loadReportFor(date: datetime):
-    if date.year == 2019 and date.mont > 1:
+    if date.year == 2019 and date.month > 1:
         # there wont be any data for this yet anyway
         return
     cmd = f"python3 mainNOW.py --year {date.year} --month {date.month} --day {date.day} --period daily"
